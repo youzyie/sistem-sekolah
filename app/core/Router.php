df6aa39020249618a496a6c0b77ec5f6501943e7
youzyie <youzyie@gmail.com>
2026-01-27 13:59:52 +0700

    feat: setup router app

---
+++ app/core/Router.php
@@ -0,0 +1,14 @@
+<?php
+namespace App\Core;
+
+
+class Router
+{
+
+    public function run(): void
+    {
+        echo'Router is running';
+    }
+}
+
+?>
---
+++ public/index.php
@@ -0,0 +1,8 @@
+<?php
+require_once './app/core/Router.php';
+
+use App\Core\Router;
+
+$router = new Router();
+$router->run();
+?>
