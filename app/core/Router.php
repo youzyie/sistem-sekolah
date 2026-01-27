8792aa42e00ec128a769aa43ba9540a6b2f3a5c2
youzyie <youzyie@gmail.com>
2026-01-27 14:30:14 +0700

    feat: implement simple router

--- app/core/Router.php
+++ app/core/Router.php
@@ -7,7 +7,23 @@ class Router
 
     public function run(): void
     {
-        echo'Router is running';
+        $method = $_SERVER['REQUEST_METHOD'];
+        $uri = parse_url($_SERVER['REQUEST_URI'],PHP_URL_PATH);
+
+        if($method == 'GET' && $uri == '/students') {
+            echo '<h1>Daftar Siswa</h1>';
+            echo '<p>Menampilkan daftar siswa</p>';
+            return;
+        }
+        if($method == 'GET' && $uri == '/students/create') {
+            echo '<h1>Tambah Siswa</h1>';
+            echo '<p>Menampilkan Form Tambah Siswa </p>';
+            return;
+        }
+
+
+        http_response_code(404);
+        echo '<h1>484 - Page Not Found</h1>';
     }
 }
 
