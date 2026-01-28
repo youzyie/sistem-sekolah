c26595ec203d97f57ef9232836f8a89e601b7781
youzyie <youzyie@gmail.com>
2026-01-28 10:16:18 +0700

    wip: implement router app"

--- app/core/Router.php
+++ app/core/Router.php
@@ -1,30 +1,46 @@
 <?php
 namespace App\Core;
-
-
+ 
+use App\Controller\StudentController;
+ 
 class Router
 {
+        private array  $routes = [];
 
-    public function run(): void
-    {
-        $method = $_SERVER['REQUEST_METHOD'];
-        $uri = parse_url($_SERVER['REQUEST_URI'],PHP_URL_PATH);
 
-        if($method == 'GET' && $uri == '/students') {
-            echo '<h1>Daftar Siswa</h1>';
-            echo '<p>Menampilkan daftar siswa</p>';
-            return;
-        }
-        if($method == 'GET' && $uri == '/students/create') {
-            echo '<h1>Tambah Siswa</h1>';
-            echo '<p>Menampilkan Form Tambah Siswa </p>';
-            return;
+        public function add(string $method, string $uri, string $controller,string $function): void
+        {
+            $this->routes[] = [
+                'method' => $method,
+                'uri' => $uri,
+                'controller' => $controller,
+                'function' => $function,
+            ];
         }
 
-
-        http_response_code(404);
-        echo '<h1>484 - Page Not Found</h1>';
-    }
+        public function run()
+        {
+            $method = $_SERVER['REQUEST_METHOD'];
+            $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+ 
+            if ($method == 'GET' && $uri == '/students') {
+                require_once '../controllers/StudentController.php';
+                $controller = new StudentController();
+                $controller->index();
+                return;
+            }
+ 
+            if ($method == 'GET' && $uri == '/students/create') {
+                echo '<h1>Tambah Siswa</h1>';
+                echo '<p>Menampilkan form tambah siswa.</p>';
+                return;
+            }
+ 
+            http_response_code(404);
+            echo '<h1>404 - Page Not Found</h1>';
+           
+        }
+   
 }
-
-?>
+ 
+ 
--- public/index.php
+++ public/index.php
@@ -4,5 +4,13 @@ require_once './app/core/Router.php';
 use App\Core\Router;
 
 $router = new Router();
+
+//register router
+$router ->add('GET','/students','StudentController', 'index');
+$router ->add('GET','/students/create','StudentController', 'create');
+$router ->add('GET','/students/{id}','StudentController', 'show');
+
+
+
 $router->run();
 ?>
